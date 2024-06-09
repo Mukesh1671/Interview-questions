a2e18d28f125af8c48850962f50fb5e946107eb5
Mukesh1671 <115939161+Mukesh1671@users.noreply.github.com>
2024-06-09 20:15:24 +0530

    Update Finding Duplicate in Array.cs

--- Finding Duplicate in Array.cs
+++ Finding Duplicate in Array.cs
@@ -1,7 +1,6 @@
 using System;
 class HelloWorld {
   static void Main() {
-    Console.WriteLine("Hello World");
     int [] duplicate = new int [5] {5,3,4,2,2};
     int [] sortedDuplicate = new int [duplicate.Length];
     Array.Copy(duplicate, sortedDuplicate, duplicate.Length);
